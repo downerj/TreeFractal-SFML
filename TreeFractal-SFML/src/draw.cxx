8d259d62f594643c2b66b3a4a9fc283346f41ac6
James Downer <jamesdanieldowner@gmail.com>
2023-11-24 23:06:49 -0700

    refactor: unnecessary namespace qualifiers

--- TreeFractal-SFML/src/draw.cxx
+++ TreeFractal-SFML/src/draw.cxx
@@ -8,7 +8,7 @@ using namespace sf;
 using namespace std;
 
 namespace fractal {
-  DrawHandler::DrawHandler(sf::RenderWindow& window, const unsigned int windowWidth, const unsigned int windowHeight) :
+  DrawHandler::DrawHandler(RenderWindow& window, const unsigned int windowWidth, const unsigned int windowHeight) :
       window(window),
       windowWidth(windowWidth),
       windowHeight(windowHeight),
@@ -28,7 +28,7 @@ namespace fractal {
       return;
     }
 
-    std::vector<Tree> trees;
+    vector<Tree> trees;
     const auto trunkStartX = windowWidth / 2.f;
     const auto trunkStartY = windowHeight / 2.f;
     constexpr auto numTrees = 3u;
