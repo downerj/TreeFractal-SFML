a78528d49c86c5098a19f1e27e6e6ad2d99a68ba
James Downer <jamesdanieldowner@gmail.com>
2023-12-12 11:27:32 -0700

    refactor: Revert equals sign syntax

--- TreeFractal-SFML/src/main.cxx
+++ TreeFractal-SFML/src/main.cxx
@@ -15,24 +15,24 @@ using namespace std;
 using namespace std::literals;
 
 auto main() -> int {
-  auto windowWidth{600u};
-  auto windowHeight{600u};
-  const auto windowMode{VideoMode{windowWidth, windowHeight}};
-  const auto windowTitle{"Tree Fractal"s};
+  auto windowWidth = 600u;
+  auto windowHeight = 600u;
+  const auto windowMode = VideoMode{windowWidth, windowHeight};
+  const auto windowTitle = "Tree Fractal"s;
 
-  auto window{RenderWindow{windowMode, windowTitle}};
-  auto view{window.getDefaultView()};
+  auto window = RenderWindow{windowMode, windowTitle};
+  auto view = window.getDefaultView();
   window.setFramerateLimit(60);
-  auto drawHandler{DrawHandler{window, windowWidth, windowHeight}};
+  auto drawHandler = DrawHandler{window, windowWidth, windowHeight};
 
   while (window.isOpen()) {
-    auto event{Event{}};
+    auto event = Event{};
     while (window.pollEvent(event)) {
-      const auto isCtrlPressed{Keyboard::isKeyPressed(Keyboard::LControl) or Keyboard::isKeyPressed(Keyboard::RControl)};
-      const auto isAltPressed{Keyboard::isKeyPressed(Keyboard::LAlt) or Keyboard::isKeyPressed(Keyboard::RAlt)};
-      const auto isWPressed{Keyboard::isKeyPressed(Keyboard::W)};
-      const auto isQPressed{Keyboard::isKeyPressed(Keyboard::Q)};
-      const auto isF4Pressed{Keyboard::isKeyPressed(Keyboard::F4)};
+      const auto isCtrlPressed = Keyboard::isKeyPressed(Keyboard::LControl) or Keyboard::isKeyPressed(Keyboard::RControl);
+      const auto isAltPressed = Keyboard::isKeyPressed(Keyboard::LAlt) or Keyboard::isKeyPressed(Keyboard::RAlt);
+      const auto isWPressed = Keyboard::isKeyPressed(Keyboard::W);
+      const auto isQPressed = Keyboard::isKeyPressed(Keyboard::Q);
+      const auto isF4Pressed = Keyboard::isKeyPressed(Keyboard::F4);
       if ((isCtrlPressed and (isWPressed or isQPressed)) or (isAltPressed and isF4Pressed)) {
         window.close();
         break;
