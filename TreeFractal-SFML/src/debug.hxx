d1768827a74423a6d400adefa6928a1e6ace2884
James Downer <jamesdanieldowner@gmail.com>
2024-01-17 15:46:22 -0700

    add: Debug log commands

--- TreeFractal-SFML/src/debug.hxx
+++ TreeFractal-SFML/src/debug.hxx
@@ -3,7 +3,13 @@
 
 #ifdef DEBUG
 #include <iostream>
-#define DEBUG_LOG(x) do { std::cout << x << std::endl; } while (0);
+#define DEBUG_LOG(x) do { std::cout << x; } while (false);
+#define DEBUG_LINE(x) do { std::cout << x << std::endl; } while (false);
+#define DEBUG_NEWLINE() do { std::cout << std::endl; } while (false);
+#else
+#define DEBUG_LOG(x) // no-op
+#define DEBUG_LINE(x) // no-op
+#define DEBUG_NEWLINE() // no-op
 #endif // DEBUG
 
 #endif // DEBUG_HXX
